d7e70f69323b8102e5a44c0a56d97ef88d65653e
karur4n <karuran.f@gmail.com>
2015-01-17 23:26:00 +0900

    [Add] add Sample.java
    enum を使ってみる

---
+++ wawawa/Sample.java
@@ -0,0 +1,11 @@
+package wawawa;
+
+import consts.MenuType;
+
+public class Sample {
+
+  public static void main(String[] args) {
+    MenuType menu = MenuType.TASK_LIST;
+    System.out.println(menu);
+  }
+}
